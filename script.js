b9da59575f889a0dd3c8f5a18437d2efe8ce53c6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-06-12 13:48:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ input.addEventListener("keydown", isKeyDown)
 var activeTetromino = []
 var cubeArray = [4,5,14,15,"yellow","cube"];
 var stickArray = [4,14,24,34,"blue","stick","vertical"];
-var elArray = [4,14,24,25,"orange","el","vertical"];
-var reverseElArray = [4,14,23,24,"cyan","reverseEl","vertical"];
+var elArray = [4,14,24,25,"orange","el","one"];
+var reverseElArray = [4,14,23,24,"cyan","reverseEl","one"];
 var esArray = [4,5,13,14,"green","es","horizontal"];
 var reverseEsArray = [4,5,15,16,"grey","reverseEs","horizontal"];
-var teeArray = [5,14,15,16,"purple","tee","knobUp"]
-var finishedArrary = []
+var teeArray = [5,14,15,16,"purple","tee","one"]
+var newBlock = false;
+var corbin = 0;
+var finishedArray = []
 function makeButtons() {
   for (var i = 0; i < 200; i++) {
       var btn = document.createElement("button");
@@ -22,12 +24,12 @@ function makeButtons() {
       document.getElementById(i).textContent = i
   }//end For loop
   //makeCube()
- 
-  for(i=0 ; i<6 ; i++){
-    activeTetromino[i] = reverseEsArray[i]
-    console.log(activeTetromino[i])
+  for(i=0 ; i < 7 ; i++){
+     activeTetromino[i] = teeArray[i]
   }
+ 
   placeTetromino()
+   console.log(activeTetromino)
 }//end function makeButton 
 function isKeyDown(e){
   if(e.code == "ArrowLeft"){
@@ -43,10 +45,13 @@ function isKeyDown(e){
     moveTetrominoDown(activeTetromino)
   }
 }
+
+function gravity(){
+  moveTetrominoDown()
+}
 function placeTetromino(){
   for(i=0; i <4; i++){
       document.getElementById(activeTetromino[i]).style.backgroundColor = activeTetromino[4]
-    
     }
 }
 function moveTetrominoLeft(){
@@ -68,94 +73,45 @@ function moveTetrominoDown(){
   //Turn Black
   //add 10
   //change to Tetromino Color
+  
   for(i=3 ; i>-1 ; i--){
     document.getElementById(activeTetromino[i]).style.backgroundColor = "black"
     activeTetromino[i] += 10
     document.getElementById(activeTetromino[i]).style.backgroundColor = activeTetromino[4]
   }
-  for(i=3 ; i> -1 ; i--){
-    if(activeTetromino[i] + 10 >200){
-      resolveStoppedTetromino();
+  if((activeTetromino[3]+10 > 200)){
+     newBlock = true;
+  }
+  for(i=0 ; i < finishedArray.length; i++){
+    if(finishedArray.includes(activeTetromino[3]+10)){
+      newBlock = true;
     }
   }
-  for(i=0 ; i < finishedArrary.length; i++){
-    if(finishedArrary.includes(activeTetromino[3]+10)){
-      resolveStoppedTetromino()
+ 
+  if(newBlock){
+    for(i = 0; i<4 ; i++){
+      finishedArray.push(activeTetromino[i])
     }
-  }
-}
-function resolveStoppedTetromino(){
-  for(i=0 ; i<4 ; i++){
-    finishedArrary.push(activeTetromino[i])
-  }
-  for(i=0 ; i<6 ; i++){
-    activeTetromino[i] = cubeArray[i];
-  }
-  placeTetromino()
-}
-function rotateTetromino(){
- if(activeTetromino[5] == "stick"){
-   rotateStick(activeTetromino)
- }else if(activeTetromino[5] == "el"){
-   rotateEl(activeTetromino)
- }else if(activeTetromino[5] == "reverseEl"){
-   rotateReverseEl(activeTetromino)
- }else if(activeTetromino[5] == "es"){
-   rotateEs(activeTetromino)
- }else if(activeTetromino[5] == "reverseEs"){
-    rotateReverseEs(activeTetromino)
- }else if(activeTetromino[5]== "tee"){
-   rotateTee(activeTetromino)
- }
+    console.log(finishedArray)
+    if(corbin == 0){
+      for(i=0 ; i < 6 ; i++){
+          activeTetromino[i] = elArray[i]
+        }
+      corbin += 1
+    }else{
+       for(i=0 ; i < 6 ; i++){
+          activeTetromino[i] = reverseElArray[i]
+        }
+    }
     
-}
-function rotateStick(){
-   if(activeTetromino[6]=="vertical"){
-    document.getElementById(activeTetromino[0]).style.backgroundColor = "black"
-    activeTetromino[0] += 9
-    document.getElementById(activeTetromino[0]).style.backgroundColor = activeTetromino[4]
-
-   document.getElementById(activeTetromino[2]).style.backgroundColor = "black"
-    activeTetromino[2] -= 9
-    document.getElementById(activeTetromino[2]).style.backgroundColor = activeTetromino[4]
-
-   document.getElementById(activeTetromino[3]).style.backgroundColor = "black"
-    activeTetromino[3] -= 18
-    document.getElementById(activeTetromino[3]).style.backgroundColor = activeTetromino[4]
-    activeTetromino[6] = "horizontal"
-  }else{
-     document.getElementById(activeTetromino[0]).style.backgroundColor = "black"
-    activeTetromino[0] -= 9
-    document.getElementById(activeTetromino[0]).style.backgroundColor = activeTetromino[4]
-
-     document.getElementById(activeTetromino[2]).style.backgroundColor = "black"
-    activeTetromino[2] += 9
-    document.getElementById(activeTetromino[2]).style.backgroundColor = activeTetromino[4]
-
-   document.getElementById(activeTetromino[3]).style.backgroundColor = "black"
-    activeTetromino[3] += 18
-    document.getElementById(activeTetromino[3]).style.backgroundColor = activeTetromino[4]
-    activeTetromino[6] = "vertical"
+    placeTetromino(activeTetromino)
+    newBlock = false
   }
-}
-function rotateEl(){
-  
-}
-function rotateReverseEl(){
-  
-}
-function  rotateEs() {
-  
-}
-function  rotateReverseEs() {
-  
-}
-function rotateTee(){
-  
-}
+}//end function
+
 function off(){
   clearInterval(myGravity)
 }
 function on(){
-  myGravity = setInterval(moveTetrominoDown, 500)
+  myGravity = setInterval(gravity,800);
 }
